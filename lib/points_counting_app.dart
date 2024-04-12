592242b63c4a341dab8b2b0f81537788cd982bef
Arno Massart <arno.massart@outlook.com>
2024-04-12 18:11:26 +0200

    Deleted some unused code

--- lib/points_counting_app.dart
+++ lib/points_counting_app.dart
@@ -408,29 +408,3 @@ class _PointsCountingAppState extends State<PointsCountingApp> {
     );
   }
 }
-
-enum IconLabel {
-  smile('Smile', Icons.sentiment_satisfied_outlined),
-  cloud(
-    'Cloud',
-    Icons.cloud_outlined,
-  ),
-  brush('Brush', Icons.brush_outlined),
-  heart('Heart', Icons.favorite);
-
-  const IconLabel(this.label, this.icon);
-  final String label;
-  final IconData icon;
-}
-
-enum ColorLabel {
-  blue('Blue', Colors.blue),
-  pink('Pink', Colors.pink),
-  green('Green', Colors.green),
-  yellow('Orange', Colors.orange),
-  grey('Grey', Colors.grey);
-
-  const ColorLabel(this.label, this.color);
-  final String label;
-  final Color color;
-}
